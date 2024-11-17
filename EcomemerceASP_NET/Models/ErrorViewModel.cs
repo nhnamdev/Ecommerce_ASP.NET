0b61749a44f60956730ed87ebe53ffda8db47437
nam <22130173@st.hcmuaf.edu.vn>
2024-11-17 16:33:18 +0700

    init

---
+++ EcomemerceASP_NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcomemerceASP_NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
