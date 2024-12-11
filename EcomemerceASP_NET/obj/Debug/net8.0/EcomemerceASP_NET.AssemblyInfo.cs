e1e4fbd5b2ec1b51f315fe36da390c7a9a126be3
nam <22130173@st.hcmuaf.edu.vn>
2024-12-11 20:56:44 +0700

    ...

--- EcomemerceASP_NET/obj/Debug/net8.0/EcomemerceASP_NET.AssemblyInfo.cs
+++ EcomemerceASP_NET/obj/Debug/net8.0/EcomemerceASP_NET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcomemerceASP_NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b663e1b512526154371f0f3c683ff39e08ccb03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944cd1457880ad20d79c1f24d1638f1547f6ca84")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcomemerceASP_NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcomemerceASP_NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
