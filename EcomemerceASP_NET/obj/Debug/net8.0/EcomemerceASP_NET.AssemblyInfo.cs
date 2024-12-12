a5b47c55f0172ecaac792b3195564e0e5751b2e2
Le Vu Hung <22130091@.edu.vn>
2024-12-12 18:40:19 +0700

    commit

--- EcomemerceASP_NET/obj/Debug/net8.0/EcomemerceASP_NET.AssemblyInfo.cs
+++ EcomemerceASP_NET/obj/Debug/net8.0/EcomemerceASP_NET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcomemerceASP_NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e4fbd5b2ec1b51f315fe36da390c7a9a126be3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd7f4618ca88f9fe24b8ca8fb273900a26baa5b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcomemerceASP_NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcomemerceASP_NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
